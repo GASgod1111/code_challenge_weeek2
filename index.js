91d912533b4a63d24819aea944f1e340c72d9e73
GASgod1111 <reagan.peter@student.moringaschool.com>
2023-07-03 01:50:23 +0300

    done

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded',()=>{
+    const voteButtons = document.querySelectorAll('.votebuttons');
+
     fetchAnimal()
 })
 function fetchAnimal(){
@@ -22,3 +24,27 @@ function displayCharacter(character){
     display.appendChild(div)
 
 }
+voteButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault(); // Prevent the default form submission behavior
+      const animalName = button.dataset.animal;
+      const animal =character.find(a => a.name === animalName);
+      if (animal) {
+        animal.vote++;
+        displayAnimal(animal);
+        updateVotesOnServer(character); // Update votes on the server
+      }
+    });
+  });
+
+resetMybutton.addEventListener("click", () => {
+fetch("http://localhost:3000/character")
+  .then(resp => resp.json())
+  .then(character => {
+    character.forEach(character => {
+      character.vote = 0;
+    });
+    displayAnimal(characters[0]);
+    updateVotesOnServer(characters); // Update votes on the server
+  });
+});
