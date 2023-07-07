802bf3123c6d004bb3b098f640979c61fe334309
GASgod1111 <135597419+GASgod1111@users.noreply.github.com>
2023-07-07 11:06:48 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,63 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const voteButtons = document.querySelectorAll('.votebuttons');
+let voteButtons;
 
-    fetchAnimal()
-})
-function fetchAnimal(){
-    fetch('http://localhost:3000/characters')
-   .then(resp=>resp.json()) 
-   .then(data=>iterateCharacter(data))
-}
-function iterateCharacter(data){
-    data.forEach(character=>{
-        displayCharacter(character);
-    }); 
-}
-function displayCharacter(character){
-    const display = document.getElementById("displayAnimals")
-    const  div=document.createElement('div')
-    div.innerHTML=`
-    <div>
-        <img src="${character.image}">
-    </div>
-    `
-    display.appendChild(div)
-
-}
-voteButtons.forEach(button => {
+document.addEventListener('DOMContentLoaded', () => {
+  voteButtons = document.querySelectorAll('.voteButtons');
+  fetchAnimal();
+  voteButtons.forEach(button => {
     button.addEventListener("click", (event) => {
-      event.preventDefault(); // Prevent the default form submission behavior
-      const animalName = button.dataset.animal;
-      const animal =character.find(a => a.name === animalName);
-      if (animal) {
-        animal.vote++;
-        displayAnimal(animal);
-        updateVotesOnServer(character); // Update votes on the server
-      }
+
     });
   });
 
-resetMybutton.addEventListener("click", () => {
-fetch("http://localhost:3000/character")
-  .then(resp => resp.json())
-  .then(character => {
-    character.forEach(character => {
-      character.vote = 0;
-    });
-    displayAnimal(characters[0]);
-    updateVotesOnServer(characters); // Update votes on the server
+  const reset = document.getElementById("reset");
+  reset.addEventListener("click", () => {
   });
+});
+
+function fetchAnimal() {
+  fetch('http://localhost:3000/characters')
+    .then(resp => resp.json())
+    .then(data => iterateCharacter(data));
+}
+
+function iterateCharacter(data) {
+  data.forEach(character => {
+    displayCharacter(character);
+  });
+}
+
+function displayCharacter(character) {
+  const display = document.getElementById("displayAnimals");
+  const div = document.createElement('div');
+  div.innerHTML = `
+    <div>
+      <img src="${character.image}">
+    </div>
+  `;
+  display.appendChild(div);
+}
+
+voteButtons.forEach(button => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    const animalName = button.dataset.animal;
+    const animal = characters.find(a => a.name === animalName);
+    if (animal) {
+      animal.vote++;
+      displayAnimal(animal);
+      updateVotesOnServer(characters);
+    }
+  });
+});
+
+reset.addEventListener("click", () => { 
+  fetch("http://localhost:3000/characters")
+    .then(resp => resp.json())
+    .then(characters => {
+      characters.forEach(character => {
+        character.vote = 0;
+      });
+      displayAnimal(characters[0]);
+      updateVotesOnServer(characters);
+    });
 });
